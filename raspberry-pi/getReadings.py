81bd0f2f0d88e2a17538d74c02fcddb69daa2f79
Daniel C <danclayson@gmail.com>
2020-08-10 11:22:55 -0600

    Get raspberrypi serial number for deviceID

--- raspberry-pi/getReadings.py
+++ raspberry-pi/getReadings.py
@@ -5,10 +5,23 @@ import requests
 
 readingTimeStampInMs = round(datetime.datetime.now().timestamp() * 1000)
 
-DEVICE_ID = "margosGreenhouse"
-SERVER = "https://greenhouse.clayson.io/.netlify"
+SERVER = "https://greenhouse.clayson.io/.netlify/functions"
 
-def getTemperatureSensorValues():
+def getRaspberryPiSerial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+
+  return cpuserial
+
+def getTemperatureSensorValues(deviceId):
     print("Getting Temperature Values")
     valueList = []
 
@@ -16,7 +29,7 @@ def getTemperatureSensorValues():
 
     for folder in subfolders:
         try:
-            value = {"deviceId": DEVICE_ID, "sensorId": None, "type": "t", "value": None, "time": readingTimeStampInMs}
+            value = {"deviceId": deviceId, "sensorId": None, "type": "t", "value": None, "time": readingTimeStampInMs}
 
             with open(folder + "/name", 'r') as name:
                 value["sensorId"] = name.readline().strip()
@@ -32,7 +45,7 @@ def getTemperatureSensorValues():
 
     return valueList
 
-def getDHTSensorValues():
+def getDHTSensorValues(deviceId):
     print("Getting DHT Values")
     valueList = []
 
@@ -40,13 +53,14 @@ def getDHTSensorValues():
     DHT_SENSOR = Adafruit_DHT.DHT22
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-    valueList.append({"deviceId": DEVICE_ID, "sensorId": "DHT22_T", "type": "t", "value": temperature, "time": readingTimeStampInMs})
-    valueList.append({"deviceId": DEVICE_ID, "sensorId": "DHT22_H", "type": "h", "value": humidity, "time": readingTimeStampInMs})
+    valueList.append({"deviceId": deviceId, "sensorId": "DHT22_T", "type": "t", "value": temperature, "time": readingTimeStampInMs})
+    valueList.append({"deviceId": deviceId, "sensorId": "DHT22_H", "type": "h", "value": humidity, "time": readingTimeStampInMs})
     return valueList
 
+DEVICE_ID = getRaspberryPiSerial()
 
-dhtReadings = getDHTSensorValues()
-temperatureReadings = getTemperatureSensorValues()
+dhtReadings = getDHTSensorValues(DEVICE_ID)
+temperatureReadings = getTemperatureSensorValues(DEVICE_ID)
 
 readings = [*dhtReadings, *temperatureReadings]
 
