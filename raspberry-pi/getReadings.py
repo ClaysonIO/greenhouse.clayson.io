e1f0e741749a1e79655051145b60e0905368cb84
Daniel C <danclayson@gmail.com>
2020-08-10 10:20:49 -0600

    Working on gettingReadings

---
+++ raspberry-pi/getReadings.py
@@ -0,0 +1,16 @@
+import Adafruit_DHT
+
+DEVICE_ID = "margosGreenhouse"
+DHT_PIN = 17
+
+subfolders = [ f.path for f in os.scandir("/sys/bus/w1/devices") if f.is_dir() ]
+
+for folder in subfolders:
+    print(folder)
+
+def getDHTSensorValues(PIN):
+    DHT_SENSOR = Adafruit_DHT.DHT22
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, PIN)
+    return humidity, temperature
+
+print("DONE")
