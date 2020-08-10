4d57194fe2ce9a8f485d75dfa4a4194e5e2c1087
Daniel C <danclayson@gmail.com>
2020-08-10 11:42:25 -0600

    Add readme and register sensors script

--- raspberry-pi/README.md
+++ raspberry-pi/README.md
@@ -4,3 +4,21 @@ This tool was setup to use two sensors -- a [DHT22 Humidity and Temperature sens
 and one or more [DS18B20 Waterproof Temperature Sensors](https://www.amazon.com/Eiechip-Waterproof-Temperature-Thermometer-Resistance/dp/B07MB1J43W/ref=sr_1_1_sspa?dchild=1&hvadid=77653128360992&hvbmt=be&hvdev=c&hvqmt=e&keywords=ds18b20+waterproof&qid=1597032649&sr=8-1-spons&tag=mh0b-20&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzMjU5UFhQSEIxTjQxJmVuY3J5cHRlZElkPUEwODk5OTI5R0dQNEEzMEhBQUdCJmVuY3J5cHRlZEFkSWQ9QTA2NDE4ODQxVkxXNklWNzIxNDNDJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==).
 
 Many resources were used while identifying the necessary code. Top on the list are [Raspberry Pi Humidity Sensor using the DHT22](https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/) and [Set Up a Raspberry Pi DS18B20 Temperature Sensor](https://myhydropi.com/ds18b20-temperature-sensor-on-a-raspberry-pi).
+
+Hardware Setup: 
+* All DS18B20 temperature sensors should be plugged into GPIO4
+* The DHT22 sensor should be plugged into GPIO17
+
+Software Setup: 
+* Log in to the Raspberry Pi
+* To setup the DS18B20 Temperature Sensors: 
+  * Type `sudo nano /boot/config.txt`
+    * Add the following line to the bottom: `dtoverlay=w1-gpio`
+* To setup the DHT 22 Humidity and Temperature Sensor
+  * Type the following in order:
+      * `sudo apt-get update`
+      * `sudo apt-get upgrade`
+      * `sudo apt-get install python3-dev python3-pip`
+      * `sudo python3 -m pip install --upgrade pip setuptools wheel`
+      * `sudo pip3 install Adafruit_DHT`
+* To setup the recurring jobs: 
---
+++ raspberry-pi/registerSensors.py
@@ -0,0 +1,69 @@
+import Adafruit_DHT
+import os
+import datetime
+import requests
+
+readingTimeStampInMs = round(datetime.datetime.now().timestamp() * 1000)
+
+SERVER = "https://greenhouse.clayson.io/.netlify/functions"
+
+def getRaspberryPiSerial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+
+  return cpuserial
+
+def getTemperatureSensorIds():
+    print("Getting Temperature IDs")
+    valueList = []
+
+    subfolders = [ f.path for f in os.scandir("/sys/bus/w1/devices") if f.is_dir() ]
+
+    for folder in subfolders:
+        try:
+            with open(folder + "/name", 'r') as name:
+                value = name.readline().strip()
+                if value is not None:
+                    valueList.append({"sensorId": value, "type": "t"})
+
+        except FileNotFoundError:
+            print("")
+
+    return valueList
+
+def getDHTSensorValues(deviceId):
+    print("Getting DHT IDs")
+    valueList = []
+
+    DHT_PIN = 17
+    DHT_SENSOR = Adafruit_DHT.DHT22
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+    if humidity is not None and temperature is not None:
+        valueList.append({"sensorId": "DHT22_H", "type": "h")
+        valueList.append({"sensorId": "DHT22_T", "type": "t")
+
+    return valueList
+
+DEVICE_ID = getRaspberryPiSerial()
+
+dhtSensors = getDHTSensorValues(DEVICE_ID)
+temperatureSensors = getTemperatureSensorValues(DEVICE_ID)
+
+deviceObject = {
+    "deviceId": DEVICE_ID,
+    "sensors": [*dhtSensors, *temperatureSensors]
+}
+
+r = requests.post(SERVER + "/registerSensors", json=deviceObject)
+
+print(r.status_code)
+print("DONE")
