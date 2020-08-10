8a0970efc7ca893a19fc89e62d8258f15de7265b
Daniel Clayson <daniel@clayson.io>
2020-08-10 17:30:52 +0100

    Read temperature

--- raspberry-pi/getReadings.py
+++ raspberry-pi/getReadings.py
@@ -1,4 +1,5 @@
 import Adafruit_DHT
+import os
 
 DEVICE_ID = "margosGreenhouse"
 DHT_PIN = 17
@@ -7,6 +8,14 @@ subfolders = [ f.path for f in os.scandir("/sys/bus/w1/devices") if f.is_dir() ]
 
 for folder in subfolders:
     print(folder)
+
+    try:
+        temp = open(folder + "/temperature")
+        print(temp.readlines())
+    except FileNotFoundError:
+        print("File Not Found")
+    finally:
+        temp.close()
 
 def getDHTSensorValues(PIN):
     DHT_SENSOR = Adafruit_DHT.DHT22
